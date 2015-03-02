489f8f6ef03009bf73e9f65bfe4fa25d4a6a9c8c
KalahariKanga <djfhill@live.co.uk>
2015-03-02 12:20:56 +0000

    circle/rectangle collision

--- MaskCircle.cpp
+++ MaskCircle.cpp
@@ -18,7 +18,13 @@ MaskCircle::~MaskCircle()
 bool MaskCircle::checkCollision(Mask* other)
 {
 	if (other->shape == MASK_SHAPE_RECTANGLE)
-		return 0;
+	{
+		float nearestX = clamp(x,other->x-other->xr,other->x+other->xr);
+		float nearestY = clamp(y,other->y-other->yr,other->y+other->yr);
+		float dx = nearestX - x;
+		float dy = nearestY - y;
+		return dx*dx + dy*dy < r*r;
+	}
 	if (other->shape == MASK_SHAPE_CIRCLE)
 	{
 		if ((Vector2(x, y) - Vector2(other->x, other->y)).getLength() < r + ((MaskCircle*)other)->r)
